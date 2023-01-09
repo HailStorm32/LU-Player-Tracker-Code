1b7a613f3422907ce6925e069ce0157ec4b43291
Demetri <demetriv.s.7@gmail.com>
2023-01-08 17:58:47 -0800

    Added more code for parsing JSON

--- Components/ledControl/ledControl.c
+++ Components/ledControl/ledControl.c
@@ -31,26 +31,131 @@ void ledUpdateTask()
 {
     msgInfoPtr_t jsonMsgInfo;
     cJSON* parsedWorldStatuses;
+    cJSON* worldID;
+    cJSON* worldName;
+    cJSON* worldPop;
 
     while (true)
     {
        //Check for new JSON
        if (xQueueReceive(mqttJsonQueue, &jsonMsgInfo, (TickType_t)25))
        {
-            ESP_LOGD(LED_CTRL_LOG_TAG, "OUT: \n %s\n\n", jsonMsgInfo->msgPtr);
+            ESP_LOGD(LED_CTRL_LOG_TAG, "JSON from queue: \n %s\n\n", jsonMsgInfo->msgPtr);
            
             //Parse the JSON
             parsedWorldStatuses = cJSON_Parse(jsonMsgInfo->msgPtr);
+
+            //Report if there is an error in the JSON
+            if (parsedWorldStatuses == NULL)
+            {
+                ESP_LOGE(LED_CTRL_LOG_TAG, "ERROR: Failed to parse JSON");
+                
+                const char* error_ptr = cJSON_GetErrorPtr();
+                if (error_ptr != NULL)
+                {
+                    ESP_LOGE(LED_CTRL_LOG_TAG, "|__Error is before: %s", error_ptr);
+                }
+                continue;
+            }
 
             //Free the unparsed JSON struct that came in the queue
             free(jsonMsgInfo->msgPtr);
             free(jsonMsgInfo);
             jsonMsgInfo = NULL;
             
-            //TODO:
-            // Cycle through the total number of worlds
-            // and check each world ID from the JSON
+            //Cycle through all the world ID objects
+            cJSON_ArrayForEach(worldID, parsedWorldStatuses)
+            {
+                //Make sure the JSON is formated like we expect
+                if(!cJSON_IsObject(worldID))
+                {
+                    ESP_LOGE(LED_CTRL_LOG_TAG, "ERROR: Expected different JSON");
+                    
+                    //Move to next item
+                    continue;
+                }
 
+                //Get world name and population
+                worldName = cJSON_GetObjectItemCaseSensitive(worldID, "name");
+                worldPop = cJSON_GetObjectItemCaseSensitive(worldID, "pop");
+
+                //Error check
+                if(!cJSON_IsNumber(worldPop))
+                {
+                    ESP_LOGE(LED_CTRL_LOG_TAG, "ERROR: Expected different JSON\n|_Expected a number for worldPop, got somthing else");
+                    
+                    //Move to next item
+                    continue;
+                }
+                if(!cJSON_IsString(worldName))
+                {
+                    ESP_LOGE(LED_CTRL_LOG_TAG, "ERROR: Expected different JSON\n|_Expected a string for worldName, got somthing else");
+                    
+                    //Move to next item
+                    continue;
+                }
+
+                //Update each world LED
+                switch (atoi(worldID->string))
+                {
+                case 1000: //Venture Explorer
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: Venture Explorer, worldID triggered: %s", worldID->string);
+                    break;
+                case 1100: case 1101: case 1102: case 1001: case 1150: case 1151: //Avant Gardens, AG Survival, Spider Queen Battle, Return to VE, Block Yard, Avant Grove
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: Avant Gardens, worldID triggered: %s", worldID->string);
+                    break;
+                case 1200: case 1203: case 1204: case 1250: case 1251: //Nimbus Station, Vertigo Loop Racetrack, Battle of NS, Nimbus Rock, Nimbus Isle
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: Nimbus Station, worldID triggered: %s", worldID->string);
+                    break;
+                case 1201: //Pet Cove
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: Pet Cove, worldID triggered: %s", worldID->string);
+                    break;
+                case 1300: case 1302: case 1303: case 1350: //Gnarled Forest, Canyon Cove, Keelhaul Canyon, Chantey Shantey
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: Gnarled Forest, worldID triggered: %s", worldID->string);
+                    break;
+                case 1400: case 1402: case 1403: case 1450: //Forbidden Valley, FV Dragon, Dragonmaw Chasm, Raven Bluff
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: Forbidden Valley, worldID triggered: %s", worldID->string);
+                    break;
+                case 1600: //Starbase 3001
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: Starbase 3001, worldID triggered: %s", worldID->string);
+                    break;
+                case 1601: //Deep Freeze
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: Deep Freeze, worldID triggered: %s", worldID->string);
+                    break;
+                case 1602: //Robot City
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: Robot City, worldID triggered: %s", worldID->string);
+                    break;
+                case 1603: //Moon Base
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: Moon Base, worldID triggered: %s", worldID->string);
+                    break;
+                case 1604: //Portabello
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: Portabello, worldID triggered: %s", worldID->string);
+                    break;
+                case 1700: //LEGO Club (Club Station Alpha)
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: LEGO Club (Club Station Alpha), worldID triggered: %s", worldID->string);
+                    break;
+                case 1800: //Crux Prime
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: Crux Prime, worldID triggered: %s", worldID->string);
+                    break;
+                case 1900: //Nexus Tower
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: Nexus Tower, worldID triggered: %s", worldID->string);
+                    break;
+                case 2000: case 2001: //Ninjago, Frakjaw Battle
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Updating LED: Ninjago, worldID triggered: %s", worldID->string);
+                    break;
+                
+                default: //Unsupported World ID
+                    ESP_LOGI(LED_CTRL_LOG_TAG, "Unsupported World ID, no LED to update, worldID triggered: %s", worldID->string);
+                    break;
+                }
+            }
+            
+            //Recursively free the parsed JSON
+            cJSON_Delete(parsedWorldStatuses);
+            parsedWorldStatuses = NULL;
+            worldID = NULL;
+            worldName = NULL;
+            worldPop = NULL;
        }
     }
     
@@ -58,3 +163,18 @@ void ledUpdateTask()
     //free(receivedData);
     vTaskDelete(NULL);
 }
+
+/* void freeItem(cJSON *item) {
+    if ((item != NULL) && (item->child != NULL))
+    {
+        cJSON_Delete(item->child);
+    }
+    if ((item->valuestring != NULL) && !(item->type & cJSON_IsReference))
+    {
+        free(item->valuestring);
+    }
+    if ((item->string != NULL) && !(item->type & cJSON_StringIsConst))
+    {
+        free(item->string);
+    }
+} */
